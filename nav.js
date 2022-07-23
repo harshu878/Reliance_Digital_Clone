d69ec7933e2401776f8f877d616ebf34e1aab488
ahlawatdeepak <deepakahlawat735729@gmail.com>
2022-07-23 15:41:12 +0530

    done

--- nav.js
+++ nav.js
@@ -1,5 +1,9 @@
 import navs from "../components/importnav.js";
+import footer from "./components/footer.js"
+import navbar from "./components/navbar.js"
 
+document.querySelector("#navbar").innerHTML=navbar();
+document.querySelector("#footer").innerHTML=footer();
 
 navs().then((data)=>{
     document.getElementById("nav").innerHTML= data
